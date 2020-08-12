3610c71defda22c7f499ecacba4e728580ccf7d1
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-08-12 02:03:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,16 @@ class ApplicationController < Sinatra::Base
   
   get '/recipes' do 
     @recipes = Recipe.all
-    erb :recipes
+    erb :index
+  end
+  
+  get '/recipes/new' do 
+    erb :new 
+  end
+  
+  post '/recipes' do 
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    redirect to "/recipes/#{@recipe.id}"
   end
 
 end
